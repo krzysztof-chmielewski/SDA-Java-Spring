7398bd3e5786663eb7a9d75fa91c1361127044cf
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2018-01-30 15:29:26 +0100

    Task 01: hello world

---
+++ 01 Hello World/src/main/java/com/kchmielewski/sda/java/spring01java/config/Config.java
@@ -0,0 +1,25 @@
+package com.kchmielewski.sda.java.spring01java.config;
+
+import com.kchmielewski.sda.java.spring01java.web.HelloWorldController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = HelloWorldController.class)
+public class Config {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
